770812dc58b777bde3326f3bcee07053e48c036d
Juan <arzzdeveloper04@gmail.com>
2024-06-20 16:39:09 -0600

    added axios.js

--- src/components/common/ModalError.jsx
+++ src/components/common/ModalError.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 const ModalError = ({ body }) => {
   return (
     <div id="popup-modal" tabIndex="-1" className="hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-screen bg-black bg-opacity-50">
-        <div className="bg-gray-700 rounded-lg shadow-lg w-full max-w-md">
+        <div className="bg-gray-900 rounded-lg shadow-lg w-full max-w-md">
             <button type="button" className="absolute top-3 right-3 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center">
                 <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                 </svg>
-                <span className="sr-only">Cerrar modal</span>
+                <span className="sr-only">Cerrar modal</span>   
             </button>
             <div className="p-4 md:p-5 text-center">
-                <svg className="mx-auto mb-4 text-gray-400 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <svg className="mx-auto mb-4 text-gray-100 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                 </svg>
-                <h3 className="mb-5 text-lg font-normal text-gray-500">{body}</h3>
+                <h3 className="mb-5 text-lg font-normal text-gray-100">{body}</h3>
                 <button type="button" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5">
                     Cerrar
                 </button>
