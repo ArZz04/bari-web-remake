4247cf1028f6ee26006f4c13799a4c272b5a943f
Juan <arzzdeveloper04@gmail.com>
2024-06-20 18:52:17 -0600

    Update ModalPrice.jsx

--- src/components/common/ModalPrice.jsx
+++ src/components/common/ModalPrice.jsx
@@ -29,7 +29,7 @@ const ModalPrice = ({ product, onClose, hidden }) => {
       id="crud-modal"
       tabIndex="-1"
       aria-hidden="true"
-      className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
     >
       <div className="relative p-4 w-full max-w-md max-h-full">
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
