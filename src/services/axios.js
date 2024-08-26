5854fdc959da7df59c043382c7e102efc83015a4
Juan Arvizu <arzzdeveloper04@gmail.com>
2024-08-26 15:32:52 -0600

    Update axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Define la URL base según el entorno
 const baseURL = process.env.NODE_ENV === 'development'
-  ? 'http://192.168.1.3:3000/api/'  // URL de desarrollo
+  ? 'http://187.224.97.238:3000/api/'  // URL de desarrollo
   : 'https://api.arzz.site/api/';  // URL de producción
 
 const axiosInstance = axios.create({
